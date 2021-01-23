bad67c6df1f5835347ac43b3e22b16daf1ef02fe
Brian Archibald <brianallenarchibald@gmail.com>
2021-01-23 14:08:37 -0600

    Refactor: Updated to Model Serializer

--- APIexchange/api/serializers.py
+++ APIexchange/api/serializers.py
@@ -2,14 +2,20 @@ from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=400)
+# #  Using regular serializer
+# class ArticleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=100)
+#     description = serializers.CharField(max_length=400)
 
-    def create(self, validated_data):
-        return Article.objects.create(validated_data)
+#     def create(self, validated_data):
+#         return Article.objects.create(validated_data)
 
-    def update(self, instance, validated_data)
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
 
+#  Using ModelSerializer, it auto creates the create and update
+class ArticleSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'description']
