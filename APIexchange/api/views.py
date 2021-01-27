9a8ef8697572f42a96b6fc5cc21327ff2479c662
Brian Archibald <brianallenarchibald@gmail.com>
2021-01-26 20:35:04 -0600

    Refactor: Added get and post w serializer

--- APIexchange/api/views.py
+++ APIexchange/api/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from .serializers import ArticleSerializer
+from rest_framework.parsers import JSONParser
 
-def Index(request):
-    return HttpResponse('this is the way')
+from .models import Article
+
+def article_list(request):
+
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(article, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        data.JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+
