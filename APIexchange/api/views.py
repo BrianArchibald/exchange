7836da9ed9a0e2730f0b625bd4296662db695037
Brian Archibald <brianallenarchibald@gmail.com>
2021-01-28 14:00:22 -0600

    Refactor: Add article details view and url, put delete added

--- APIexchange/api/views.py
+++ APIexchange/api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .serializers import ArticleSerializer
 from rest_framework.parsers import JSONParser
 
 from .models import Article
 
+@csrf_exempt
 def article_list(request):
 
     if request.method == 'GET':
@@ -21,4 +23,26 @@ def article_list(request):
         return JsonResponse(serializer.errors, status=400)
 
 
+@csrf_exempt
+def article_details(request, id):
+    try:
+        article = Articles.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data.JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=204)
 
