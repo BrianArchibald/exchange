d308e00861fa635aded167079ba34536b09098bc
Brian Archibald <brianallenarchibald@gmail.com>
2021-01-30 14:48:23 -0600

    Refactor: Browsable api , api view decorators, djr status

--- APIexchange/api/views.py
+++ APIexchange/api/views.py
@@ -1,29 +1,29 @@
+from .models import Article
 from django.shortcuts import render, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .serializers import ArticleSerializer
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-from .models import Article
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def article_list(request):
 
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(article, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     if request.method == 'POST':
-        data.JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
+        serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
 def article_details(request, id):
     try:
         article = Articles.objects.get(pk=pk)
