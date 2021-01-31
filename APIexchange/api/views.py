bab9fcdd57dc8fc69c693aca013e0357ea67e216
Brian Archibald <brianallenarchibald@gmail.com>
2021-01-31 14:46:30 -0600

    Refactor: Class based view completed.

--- APIexchange/api/views.py
+++ APIexchange/api/views.py
@@ -24,6 +24,34 @@ class ArticleList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class ArticleDetails(APIView):
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 # FUNCTION BASED API VIEWS
 '''
 @api_view(['GET', 'POST'])
