be78463cbb71e3e0103210cea6773620d0af015b
Brian Archibald <brianallenarchibald@gmail.com>
2021-02-02 17:09:52 -0600

    Refactor: Viewsets and Routers

--- APIexchange/api/views.py
+++ APIexchange/api/views.py
@@ -7,6 +7,30 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins
 from rest_framework.views import APIView
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
+
+class ArticleViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer_class = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer_class = ArticleSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
 
 # CLASS BASED VIEW
 
